4ac44e1713e59a52cba5d36d60f8a25e4803860f
tevfikkdurmus <tevfikk.durmus@gmail.com>
2025-02-10 14:56:19 +0300

    faq page fixed scrol func

--- services/webpage/index.js
+++ services/webpage/index.js
@@ -1,7 +1,7 @@
 const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
 async function getFrequentlyAskedQuestions(page = 1, lang = 'tr') {
-    const response = await fetch(`${apiUrl}/Clients/GetAllWebPage?Language=${lang}&Slug=sss`, {
+    const response = await fetch(`${apiUrl}/Clients/GetAllWebPage?Language=${lang}&Slug=sss&Pagination.Page=0&Pagination.Size=50`, {
         cache: 'no-store'
     })
     const data = await response.json()
